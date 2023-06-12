b2934628ebb3212343401be4f499aab3ac20d46e
YASH1730 <yashwantsahu3002@gmail.com>
2023-06-12 13:14:55 +0530

    fix :: new API link and some UI FIxes

--- src/components/modules/FileUpload.js
+++ src/components/modules/FileUpload.js
@@ -17,7 +17,11 @@ import DataGrid from "../utils/DataTable";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CancelIcon from "@mui/icons-material/Cancel";
 import Cloud from "../../assets/image/cloud.png";
-import { sendFile, sendFile_Questioners, sendFile_SOC2 } from "../service/service";
+import {
+  sendFile,
+  sendFile_Questioners,
+  sendFile_SOC2,
+} from "../service/service";
 const FileUpload = () => {
   const initialState = {
     SIGlight: "",
@@ -29,45 +33,55 @@ const FileUpload = () => {
   const [state, setState] = useReducer(reducer, initialState);
 
   async function handleSubmit(e) {
-    e.preventDefault();
+    try {
+      e.preventDefault();
 
-    setState({
-      type: "Set_Value",
-      payload: {
-        loading: true,
-        rows: [],
-      },
-    });
-    const FD = new FormData();
-    FD.append("SIGlight", state.SIGlight);
-    FD.append("SOC2", state.SOC2);
-    // API Call 
-    let res = "";
+      setState({
+        type: "Set_Value",
+        payload: {
+          loading: true,
+          rows: [],
+        },
+      });
+      const FD = new FormData();
+      FD.append("SIGlight", state.SIGlight);
+      FD.append("SOC2", state.SOC2);
+      // API Call
+      let res = "";
 
-    if(state.SIGlight !== "" && state.SIGlight)
-    res =  await sendFile_Questioners(FD);
-    else if(state.SOC2 !== "" && state.SOC2)
-    res =  await sendFile_SOC2(FD);
-    if (res.status === 200) {
+      if (state.SIGlight !== "" && state.SIGlight)
+        res = await sendFile_Questioners(FD);
+      else if (state.SOC2 !== "" && state.SOC2) res = await sendFile_SOC2(FD);
+      if (res.status === 200) {
+        setState({
+          type: "Set_Value",
+          payload: {
+            loading: false,
+            rows: [
+              ...res.data.Question.map((row, i) => {
+                return {
+                  id: i + 1,
+                  question: row,
+                  answer: res.data.Answer[i],
+                  risk: res.data.Answer[i],
+                };
+              }),
+            ],
+          },
+        });
+      } else {
+        window.alert("Error Occurred in APIs !!!");
+      }
+    } catch (error) {
       setState({
         type: "Set_Value",
         payload: {
           loading: false,
-          rows: [
-            ...res.data.Question.map((row, i) => {
-              return {
-                id: i + 1,
-                question: row,
-                answer: res.data.Answer[i],
-                risk: res.data.Answer[i],
-              };
-            }),
-          ],
+          rows: [],
         },
       });
-    }
-    else{
-      window.alert("Error Occurred in APIs !!!")
+      console.log("ERROR>>", error);
+      window.alert("Something Went Wrong !!!");
     }
   }
   function handleRemove(name) {
@@ -89,52 +103,57 @@ const FileUpload = () => {
           onSubmit={handleSubmit}
         >
           <Box>
-            <Typography variant="h6" sx = {{ fontSize : "1 rem",fontWeight : 700}}>File Uploading</Typography>
+            <Typography
+              variant="h6"
+              sx={{ fontSize: "1 rem", fontWeight: 700 }}
+            >
+              File Uploading
+            </Typography>
           </Box>
-            <Box className="file-upload-section flex">
-              <Box className="file-upload-input flex">
-                <Box className="flex" sx={{ alignItems: "center" }}>
-                  <Typography
-                    color={"primary"}
-                    sx={{ fontWeight: 600 }}
-                    variant="body1"
-                  >
-                    Upload Questionnaires
-                  </Typography>
-                  {state.SIGlight && <CheckCircleIcon color={"primary"} />}
-                </Box>
+          <Box className="file-upload-section flex">
+            <Box className="file-upload-input flex">
+              <Box className="flex" sx={{ alignItems: "center" }}>
+                <Typography
+                  color={"primary"}
+                  sx={{ fontWeight: 600 }}
+                  variant="body1"
+                >
+                  Upload Questionnaires
+                </Typography>
+                {state.SIGlight && <CheckCircleIcon color={"primary"} />}
+              </Box>
 
+              <FileDrop
+                files={[".xlsx", ".csv"]}
+                handleRemove={handleRemove}
+                state={state}
+                name={"SIGlight"}
+                setState={setState}
+                cssClass={"upload-section"}
+              />
+            </Box>
+            <Box className="file-upload-input flex">
+              <Box className="flex" sx={{ alignItems: "center" }}>
+                <Typography
+                  color={"primary"}
+                  sx={{ fontWeight: 600 }}
+                  variant="body1"
+                >
+                  Upload SOC-2 (pdf)
+                </Typography>
+                {state.SOC2 && <CheckCircleIcon color={"primary"} />}
+              </Box>
+              <Box>
                 <FileDrop
-                  files = {[".xlsx", ".csv"]}
+                  files={[".pdf"]}
                   handleRemove={handleRemove}
                   state={state}
-                  name={"SIGlight"}
+                  name={"SOC2"}
                   setState={setState}
                   cssClass={"upload-section"}
                 />
-              </Box>
-              <Box className="file-upload-input flex">
-                <Box className="flex" sx={{ alignItems: "center" }}>
-                  <Typography
-                    color={"primary"}
-                    sx={{ fontWeight: 600 }}
-                    variant="body1"
-                  >
-                    Upload SOC-2 (pdf)
-                  </Typography>
-                  {state.SOC2 && <CheckCircleIcon color={"primary"} />}
-                </Box>
-                <Box>
-                  <FileDrop
-                  files = {[".pdf"]}
-                  handleRemove={handleRemove}
-                    state={state}
-                    name={"SOC2"}
-                    setState={setState}
-                    cssClass={"upload-section"}
-                  />
 
-                  {/* <TextField
+                {/* <TextField
                     name={"SOC2"}
                     id="SOC2"
                     fullWidth
@@ -148,12 +167,12 @@ const FileUpload = () => {
                     type="file"
                     placeholder="Upload CSV"
                   /> */}
-                </Box>
               </Box>
             </Box>
-            <Box className="file-query-result">
-              <DataGrid state={state} />
-            </Box>
+          </Box>
+          <Box className="file-query-result">
+            <DataGrid state={state} />
+          </Box>
           <Box className="file-upload-buttons flex">
             <Button
               color="secondary"
